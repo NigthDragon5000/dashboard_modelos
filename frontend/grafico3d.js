a0e36dde71a788b6fc48f5575c53219a7d1db1db
jjcc <jjcc5000@gmail.com>
2022-02-04 10:30:30 -0500

    040222 1030

--- frontend/grafico3d.js
+++ frontend/grafico3d.js
@@ -5,9 +5,12 @@
   
 
     const scene = new THREE.Scene();
-	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
 
-	const renderer = new THREE.WebGLRenderer();
+   
+	const renderer = new THREE.WebGLRenderer(
+        {alpha:true,}
+    );
 	//renderer.setSize( window.innerWidth, window.innerHeight );
 	//document.body.appendChild( renderer.domElement );
     renderer.setSize( 1300, 680 );
@@ -15,17 +18,31 @@
     let tree = document.getElementById('tree');
     tree.appendChild( renderer.domElement );
 
-	const geometry = new THREE.IcosahedronGeometry();
-	const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+	//const geometry = new THREE.IcosahedronGeometry();
+    const geometry = new THREE.SphereBufferGeometry(.5,64,64)
+
+
+	//const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+
+
+    const material = new THREE.MeshStandardMaterial( );
+    material.metalness=0.7;
+    material.roughness=0.2;
+    material.color = new THREE.Color(0x292929);
 	const cube = new THREE.Mesh( geometry, material );
 
-    const edges = new THREE.EdgesGeometry( geometry );
-    const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+    //const edges = new THREE.EdgesGeometry( geometry );
+    //const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
  
 
 	scene.add( cube );
-    scene.add( line );
-    camera.position.z = 3;
+    //scene.add( line );
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 2;
+
+    //renderer.setSize(sizes.width,sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 
 	function animate() {
 		requestAnimationFrame( animate );
